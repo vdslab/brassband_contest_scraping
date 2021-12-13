bebdcfecbf7aba194ce73f7a128911ec538d0199
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2021-12-14 01:43:03 +0900

    rm

--- makeDbData.py
+++ makeDbData.py
@@ -16,8 +16,6 @@ with open("contest/2013-2017_chiku.json") as f:
 
 allData = shibuData+chikuData
 
-divideYearData = {"2013": [], "2014": [], "2015": [], "2016": [], "2017": []}
-
 allShortData = {}
 
 for item in allData:
